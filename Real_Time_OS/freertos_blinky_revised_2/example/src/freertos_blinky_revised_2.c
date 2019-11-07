dd7450f3df9798ee9583ca2d4a194cadec48f292
Alpit Abrol <55200468+alpitabrol@users.noreply.github.com>
2019-11-07 10:40:06 -0500

    Update freertos_blinky_revised_2.c

--- Real_Time_OS/freertos_blinky_revised_2/example/src/freertos_blinky_revised_2.c
+++ Real_Time_OS/freertos_blinky_revised_2/example/src/freertos_blinky_revised_2.c
@@ -1,5 +1,6 @@
 /* @brief FreeRTOS Blinky example
  A Simple Blinking Code with just using delays to alternatively blinking all 3 LEDs */
+/*author: alpit*/
 #include "board.h"
 #include "FreeRTOS.h"
 #include "task.h"
